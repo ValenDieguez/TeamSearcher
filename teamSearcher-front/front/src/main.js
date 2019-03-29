1f0e1b5ca12b4b28c72779621fba4366f462e179
Valenti <valendieguez89@gmail.com>
2019-03-29 17:27:37 +0100

    quasar

--- teamSearcher-front/front/src/main.js
+++ teamSearcher-front/front/src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import './styles/quasar.styl'
+import 'quasar-extras/material-icons'
+import Quasar from 'quasar'
+
+Vue.use(Quasar, {
+  config: {}
+ })
+
 Vue.config.productionTip = false
 
 new Vue({
